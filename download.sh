2c4fd4e1443698b66b0225f55fb94f886509c7a5
Jens Braeuer <jens.braeuer@commercetools.de>
2012-03-15 13:26:47 +0000

    Add script to download Jenkins. Update documentation.

---
+++ download.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ "$1" != "-d" ]; then
+    echo "Read the source Luke..."
+    exit 1
+fi
+
+set -e
+
+base="http://pkg.jenkins-ci.org/debian/binary"
+
+# prepare directories
+mkdir -p "work"
+cd "work"
+
+# download
+jenkins="1.454"
+wget "$base/jenkins_${jenkins}_all.deb"
